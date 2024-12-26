4ed77e94ae594264218c73e8c16842d2c2345994
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-12-26 19:08:12 +0100

    Add inputField

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <TitleName>Employee Manager</TitleName>
+  <!-- <InputField placeholder="First Name" type="text" />
+  <InputField placeholder="Last Name" type="text"/>
+  <InputField placeholder="Start Year" type="number"/>
+  <InputField placeholder="Address" type="text"/>
+  <label for="checkbox">Has Completed Training</label>
+  <input id="checkbox" type="checkbox"> -->
+  <!-- <InputField placeholder="" type="number"/> -->
 </template>
 
 <script lang="ts">
-import TitleName from 'src/components/layout/TitleName.vue'
+import TitleName from 'src/components/layout/TitleName.vue';
+import InputField from 'src/components/form/InputField.vue';
 export default {
   setup() {},
   components: {
     TitleName,
+    InputField
   },
 }
 </script>
---
+++ client/src/components/form/InputField.vue
@@ -0,0 +1,19 @@
+<template>
+  <input :type="type" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {},
+})
+</script>
+
+<style lang="scss" scoped></style>
