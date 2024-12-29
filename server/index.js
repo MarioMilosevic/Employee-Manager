f48d7b1d1c2f6e19cb814436f147b9a6534b35d2
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-12-29 16:34:00 +0100

    Fix prisma configuration

--- server/index.js
+++
@@ -1,7 +0,0 @@
-import app from "./server.js";
-
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`hello on http://localhost:${port}`);
-});
---
+++ server/src/index.ts
@@ -0,0 +1,6 @@
+import app from "./server";
+
+const port = 3000
+app.listen(port, (req, res) => {
+    console.log(`Server listening on port ${port}`)
+})
