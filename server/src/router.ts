569cc48b2b78de2d271fe7019565af8defebaa75
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-12-29 18:59:05 +0100

    Add cors middleware

--- server/src/router.ts
+++ server/src/router.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get('/employee', () => {})
-router.post('/employee', () => {})
-router.get('/employee:id', () => {})
-router.put('/employee:id', () => {})
-router.delete('/employee:id', () => { })
+router.get("/employee", (req, res) => {
+  res.json({message:"mario mekbuk pro 14"})
+});
+router.post("/employee", () => {});
+router.get("/employee:id", () => {});
+router.put("/employee:id", () => {});
+router.delete("/employee:id", () => {});
 
-export default router
+export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import router from "./router";
+import morgan from "morgan";
+import cors from "cors";
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200);
-  res.send("hello world");
-});
-
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
 
 export default app;
+// app.get("/", (req, res) => {
+//   res.status(200);
+//   res.send("hello world");
+// });
