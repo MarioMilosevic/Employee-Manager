5861fb24a5f175e7dc3c6883bd61b9fafa5985c4
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-12-29 17:48:53 +0100

    Add dummy routes

---
+++ server/src/router.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get('/employee', () => {})
+router.post('/employee', () => {})
+router.get('/employee:id', () => {})
+router.put('/employee:id', () => {})
+router.delete('/employee:id', () => { })
+
+export default router
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
-import express from 'express';
+import express from "express";
+import router from "./router";
+const app = express();
 
-const app = express()
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("hello world");
+});
 
-app.get('/', (req, res) => {
-    res.status(200)
-    res.send("hello world")
-})
+app.use("/api", router);
 
-export default app 
+export default app;
